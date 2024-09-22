0ba337dd423ac56f7515c097d826617523c1b2ea
Harry <ashukr3384@gmail.com>
2024-09-22 09:57:25 +0530

    𝙷𝙰𝚁𝚁𝚈 😈

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/SARKAROP123/EXTRAPLUGIN-",
+    "https://github.com/harryashu5/JNVI8",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
