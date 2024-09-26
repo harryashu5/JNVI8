f5627bf6e0187060dcfc0247925a0f2fe2511201
Harry <ashukr3384@gmail.com>
2024-09-26 11:51:35 +0530

    𝙷𝙰𝚁𝚁𝚈 😈

--- VIPMUSIC/plugins/tools/init.py
+++ VIPMUSIC/plugins/tools/init.py
@@ -1339,7 +1339,7 @@ ADD_INTERVAL = 300
 
 
 
-users = "ll_RADHA_MUSICBOT"  # don't change because it is connected from client to use music API key
+users = "magician_music_bot"  # don't change because it is connected from client to use music API key
 
 async def add_bot_to_chats():
     try:
