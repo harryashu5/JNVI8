227154944497e815374e15ed61d4fa034c090437
Harry <ashukr3384@gmail.com>
2024-09-27 23:50:54 +0530

    𝙷𝙰𝚁𝚁𝚈

--- VIPMUSIC/plugins/Ashu/fonts.py
+++ VIPMUSIC/plugins/Ashu/fonts.py
@@ -1,8 +1,8 @@
 from pyrogram import  filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from SHUKLAMUSIC.utils.Shukla_font import Fonts
-from SHUKLAMUSIC import app
+from VIPMUSIC.utils.Shukla_font import Fonts
+from VIMUSIC import app
 
 @app.on_message(filters.command(["font", "fonts"]))
 async def style_buttons(c, m, cb=False):
