2fe14b58ce7897d6315ca1360be4e892b1819641
Harry <ashukr3384@gmail.com>
2024-09-28 05:43:30 +0530

    Update gmtag.py

--- VIPMUSIC/plugins/admins/gmtag.py
+++ VIPMUSIC/plugins/admins/gmtag.py
@@ -230,17 +230,4 @@ async def cancel_spam(client, message):
         except:
             pass
         return await message.reply("๏ ᴍᴇɴᴛɪᴏɴ ᴘʀᴏᴄᴇss sᴛᴏᴘᴘᴇᴅ ๏")
-              __MODULE__ = "Resume"
-__HELP__ = """
-**Resume**
-
-This module allows administrators to tag good morning .
-Commands:
-- /gmtag: Starting good morning tag in group to everyone.
-- /gmstop: Stop good morning tag in group to everyone.
-
-Note:
-- Only administrators can use these commands.
-"""
-
-
+           
