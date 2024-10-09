aba5694f4e2bd78079492e8c93afc1863b09eff8
Harry <ashukr3384@gmail.com>
2024-10-09 22:48:04 +0530

    🍷

--- VIPMUSIC/utils/filters_func.py
+++ VIPMUSIC/utils/filters_func.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
-from SHUKLAMUSIC import app
+from VIPMUSIC import app
 from pyrogram.types import InlineKeyboardMarkup, Message
-from SHUKLAMUSIC.utils.msg_types import button_markdown_parser
-from SHUKLAMUSIC.utils.notes_func import NoteFillings
+from VIPMUSIC.utils.msg_types import button_markdown_parser
+from VIPMUSIC.utils.notes_func import NoteFillings
 from emojis import decode
 from pyrogram.types import Message
 
